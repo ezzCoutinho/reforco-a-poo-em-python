67e42095da3aa40667b2aea0bf7692cf53db36ca
ezzcoutinho <lammerlopes@hotmail.com>
2026-03-22 00:48:42 -0300

    Add Carro class to demonstrate class instantiation and methods in Python
    Introduced a new file aula02.py containing the Carro class, which includes an initialization method and an accelerate method. Demonstrated usage by creating instances of Carro and invoking their methods to showcase object-oriented programming concepts.

---
+++ aula02.py
@@ -0,0 +1,19 @@
+# métodos em instâncias de classes Python
+# hard code - é algo que foi escrito diretamente no código.
+
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f"{self.nome} está acelerando...")
+
+
+fusca = Carro("Fusca")
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro("Celta")
+print(celta.nome)
+celta.acelerar()
